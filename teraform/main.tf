e3bd966cf90363d4025cabb92e598f4cb0ed0fd4
Youssef <youssef.najeh@5thfloor.be>
2025-04-30 21:08:45 +0200

    add docker

--- teraform/main.tf
+++ teraform/main.tf
@@ -3,6 +3,29 @@ terraform {
   required_version = ">= 1.0.0"
 }
 
+# docker_install.tf
+resource "null_resource" "install_docker" {
+  triggers = {
+    always_run = timestamp() # Ensures this runs every time
+  }
+
+  provisioner "local-exec" {
+    command = <<-EOT
+      # Install Docker
+      if ! command -v docker &> /dev/null; then
+        echo "Installing Docker..."
+        curl -fsSL https://get.docker.com | sh
+        sudo usermod -aG docker $USER
+      else
+        echo "Docker already installed"
+      fi
+
+      # Start Docker service
+      sudo systemctl enable --now docker
+    EOT
+  }
+}
+
 
 resource "docker_network" "app_network" {
   name   = "n22"
@@ -12,6 +35,8 @@ resource "docker_network" "app_network" {
   lifecycle {
     ignore_changes = [name]
   }
+  depends_on = [null_resource.install_docker]
+
 }
 
 # Call Nginx Module
@@ -32,6 +57,9 @@ module "nginx" {
   jenkins_ready_check   = module.jenkins.is_ready
   sonarqube_dependency  = module.sonarqube.container_id
   jenkins_dependency    = module.jenkins.container_id
+
+  depends_on = [null_resource.install_docker]
+
 }
 
 # Call SonarQube Module
@@ -43,6 +71,9 @@ module "sonarqube" {
   postgres_user    = "sonar"
   postgres_password = "your_secure_password_here" # Change this!
   sonarqube_host   = "sonarqube"  # Must match what Nginx expects
+
+  depends_on = [null_resource.install_docker]
+
 }
 
 # Call Jenkins Module
@@ -58,6 +89,8 @@ module "jenkins" {
   jenkins_port     = 8083
   agent_port       = 50000
   docker_sock_path = "/var/run/docker.sock"  # For Docker-based builds
+  depends_on = [null_resource.install_docker]
+
 }
 
 module "keycloak" {
@@ -69,13 +102,9 @@ module "keycloak" {
   # Optional variables (using defaults)
   # admin_user = "admin"
   hostname   = "auth.craftmanpro.online"
+
+  depends_on = [null_resource.install_docker]
+
 }
 
 
-# Define dependencies between services
-resource "null_resource" "service_ordering" {
-  depends_on = [
-    module.sonarqube,  # SonarQube should be ready before Jenkins
-    module.nginx
-  ]
-}
